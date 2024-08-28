552dff651bf4b93c1efc7a76d5111c9285898505
Jonaspng <jonaspng@hotmail.com>
2024-08-28 15:59:26 +0800

    Update forum_data controller

--- backend/app/controllers/forum_data_controller.rb
+++ backend/app/controllers/forum_data_controller.rb
@@ -5,7 +5,8 @@ class ForumDataController < ApplicationController
   end
 
   def show
-    @data = ForumData.nearest_neighbors(:embedding, distance: "cosine").first(5)
+    question_embedding = params[:question_embedding]
+    @data = ForumData.nearest_neighbors(:embedding, question_embedding, distance: "cosine").first(5)
     render json: @data
   end
 end
